6bae97c9f630814da4e8772c861698203a149327
liuzhuo <liuzhuo@putao.com>
2020-07-28 16:23:51 +0800

    update

---
+++ app/src/main/java/com/lengjiye/code/networkscope/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.lengjiye.code.networkscope
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
